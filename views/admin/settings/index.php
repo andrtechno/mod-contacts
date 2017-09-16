27769b227926434f19092b3de654355021770be8
andrtechno <andrew.panix@gmail.com>
2017-09-16 20:47:10 +0300

    init

--- views/admin/settings/index.php
+++ views/admin/settings/index.php
@@ -1,35 +1,23 @@
 <?php
 
 use panix\engine\Html;
-use yii\widgets\ActiveForm;
+use panix\engine\bootstrap\ActiveForm;
 ?>
 <?php
 $form = ActiveForm::begin([
             //  'id' => 'form',
             'options' => ['class' => 'form-horizontal'],
-            'fieldConfig' => [
-                'template' => "{label}\n<div class=\"col-sm-8\">{input}</div>\n<div class=\"col-sm-8 col-sm-offset-4\">{error}</div>",
-                'labelOptions' => ['class' => 'col-sm-4 control-label'],
-            ],
         ]);
 ?>
 <div class="panel panel-default">
     <div class="panel-heading">
         <h3 class="panel-title"><?= $this->context->pageName ?></h3>
     </div>
-    <div class="panel-body panel-body-form">
+    <div class="panel-body">
         <?= $form->field($model, 'email') ?>
         <?=
         $form->field($model, 'feedback_tpl_body')->widget(panix\ext\tinymce\TinyMce::className(), [
             'options' => ['rows' => 6],
-            'clientOptions' => [
-                'plugins' => [
-                    "advlist autolink lists link charmap print preview anchor",
-                    "searchreplace visualblocks code fullscreen",
-                    "insertdatetime media table contextmenu paste"
-                ],
-                'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-            ]
         ]);
         ?>
 
