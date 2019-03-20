b35410b1dd556d46e4d4d14c1613ce81053d4e07
Andrew <andrew.panix@gmail.com>
2019-03-20 21:09:48 +0200

    init

---
+++ messages/ru/default.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'MODULE_NAME' => 'Контакты',
+    'MODULE_DESC' => 'Управление и настройка контактов',
+    'SUCCESS_SEND_FORM' => 'Ваше сообщение успешно отправлено!',
+    'FB_FORM_NAME' => 'Форма обратной связи',
+    'FB_FROM_SUBJECT' => 'Сообщение с сайта {sitename} от {user_name}',
+];
