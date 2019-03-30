c4929d0002ef20b718ad135909f95ec6ecbdd67b
Andrew <andrew.panix@gmail.com>
2019-03-30 12:50:42 +0200

    111

---
+++ migrations/m190330_104053_contacts_map.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Generation migrate by PIXELION CMS
+ *
+ * @author PIXELION CMS development team <dev@pixelion.com.ua>
+ * @link http://pixelion.com.ua PIXELION CMS
+ *
+ * Class m190330_104053_contacts_map
+ */
+
+use panix\engine\db\Migration;
+use panix\mod\contacts\models\Maps;
+
+class m190330_104053_contacts_map extends Migration {
+
+    public function up() {
+        $this->createTable(Maps::tableName(), [
+            'id' => $this->primaryKey()->unsigned(),
+            'name' => $this->string(255)->notNull(),
+            'zoom' => $this->smallInteger()->unsigned(),
+            'height' => $this->string(5)->notNull(),
+            'width' => $this->string(5)->notNull(),
+            'center' => 'point',
+            'type' => "ENUM('roadmap', 'satellite', 'hybrid', 'terrain')",
+            'drag' => $this->boolean()->notNull(),
+            'scrollwheel' => $this->boolean()->notNull(),
+            'transitLayer' => $this->boolean()->notNull(),
+            'trafficLayer' => $this->boolean()->notNull(),
+            'fullscreenControl' => $this->boolean()->notNull(),
+            'streetViewControl' => $this->boolean()->notNull(),
+            'mapTypeControl' => $this->boolean()->notNull(),
+            'zoomControl' => $this->boolean()->notNull(),
+            'scaleControl' => $this->boolean()->notNull(),
+            'rotateControl' => $this->boolean()->notNull(),
+            'auto_show_routers' => $this->boolean()->notNull(),
+        ], $this->tableOptions);
+    }
+
+    public function down() {
+        echo "m190330_104053_contacts_map cannot be reverted.\n";
+        $this->dropTable(Maps::tableName());
+        return false;
+    }
+
+}
---
+++ migrations/m190330_104130_contacts_map_markers.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Generation migrate by PIXELION CMS
+ *
+ * @author PIXELION CMS development team <dev@pixelion.com.ua>
+ * @link http://pixelion.com.ua PIXELION CMS
+ *
+ * Class m190330_104130_contacts_map_markers
+ */
+
+use panix\engine\db\Migration;
+use panix\mod\contacts\models\Markers;
+
+class m190330_104130_contacts_map_markers extends Migration {
+
+    // Use up()/down() to run migration code without a transaction.
+    public function up() {
+        $this->createTable(Markers::tableName(), [
+            'id' => $this->primaryKey()->unsigned(),
+            'map_id' => $this->string(255)->null(),
+            'coords' => 'POINT',
+            'name' => $this->text()->null(),
+            'icon_file' => $this->string(255)->null(),
+            'icon_file_offset_x' => $this->string(5)->null(),
+            'icon_file_offset_y' => $this->string(5)->null(),
+            'icon_content' => $this->string(255)->null(),
+            'hint_content' => $this->string(255)->null(),
+            'content_body' => $this->text()->null(),
+            'content_footer' => $this->string(255)->null(),
+            'opacity' => $this->float(255)->defaultValue(1),
+
+        ], $this->tableOptions);
+        $this->createIndex('map_id', Markers::tableName(), 'map_id');
+    }
+
+    public function down() {
+        echo "m190330_104130_contacts_map_markers cannot be reverted.\n";
+        $this->dropTable(Markers::tableName());
+        return false;
+    }
+
+}
