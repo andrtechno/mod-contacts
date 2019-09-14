f2b466f41df9e384aa94d78a1f943178b394dbef
Andrew <andrew.panix@gmail.com>
2019-09-14 13:32:02 +0300

    init

--- controllers/admin/MarkersController.php
+++ controllers/admin/MarkersController.php
@@ -56,10 +56,7 @@ class MarkersController extends AdminController
         $post = Yii::$app->request->post();
         if ($model->load($post) && $model->validate()) {
             $model->save();
-            Yii::$app->session->setFlash('success', Yii::t('app', ($isNew) ? 'SUCCESS_CREATE' : 'SUCCESS_UPDATE'));
-            $redirect = (isset($post['redirect'])) ? $post['redirect'] : Yii::$app->request->url;
-            if (!Yii::$app->request->isAjax)
-                return Yii::$app->getResponse()->redirect($redirect);
+            $this->redirectPage($isNew, $post);
         }
         return $this->render('update', [
             'model' => $model,
